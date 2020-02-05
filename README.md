09baa18e1ec6514ee57d415ec4176a01f77873da
hieupv <hieupv@codersvn.com>
2020-02-05 14:52:58 +0700

    fetch latest version of dependency while install

---
+++ README.md
@@ -0,0 +1,30 @@
+# Vicoders CLI Handler
+
+- [Vicoders CLI Handler](#vicoders-cli-handler)
+  - [Getting Started](#getting-started)
+  - [Steps](#steps)
+    - [Add NPM dependency](#add-npm-dependency)
+
+## Getting Started
+
+Fork this repo to start build new command
+
+## Steps
+
+### Add NPM dependency
+
+```javascript
+
+/**
+ * Add package dependency to package.json
+ *
+ * @param name
+ * @param options
+ * @param options.type NodeDependencyType
+ * @param options.version string
+ * @param options.overwrite boolean
+ */
+addDependency(name, options)
+```
+
+
--- src/utility/add-dependency.ts
+++ src/utility/add-dependency.ts
@@ -1,16 +1,29 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
 import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
+import { exec } from 'shelljs';
 
+/**
+ * Add package dependency to package.json
+ *
+ * @param name
+ * @param options
+ * @param options.type NodeDependencyType
+ * @param options.version string
+ * @param options.overwrite boolean
+ */
 export function addDependency(name: string, options?: any): Rule {
   return (host: Tree) => {
     options = options || {};
     addPackageJsonDependency(host, {
       name: name,
       type: options.type ? options.type : NodeDependencyType.Default,
-      version: options.version ? options.version : 'latest',
+      version: options.version
+        ? options.version
+        : exec(`npm view ${name} version`)
+            .toString()
+            .replace('\n', ''),
       overwrite: options.override ? options.override : false
     });
-
     return host;
   };
 }
